67f1691b098e0d0df8a9eb7e3756c38715fae2c6
astnio <github@mail.astn.io>
2025-08-26 08:51:44 -0700

    Added Sass. Added some styling. Added import aliases.

--- src/components/Appbar.svelte
+++ src/components/Appbar.svelte
@@ -2,9 +2,30 @@
 </script>
 
 <nav>
+  <p>LOGO</p>
   <ol>
     <li><a href="/">Home</a></li>
     <li><a href="/about">About us</a></li>
     <li><a href="plan">Create your plan</a></li>
   </ol>
 </nav>
+
+<style lang="scss">
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    width: 100%;
+  }
+
+  ol {
+    display: flex;
+    gap: 1rem;
+
+    list-style-type: none;
+
+    margin: 0;
+    padding: 0;
+  }
+</style>
