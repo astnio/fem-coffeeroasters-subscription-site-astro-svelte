302c5e9acb274abf4b6caa86a540be827877fa27
astnio <github@mail.astn.io>
2025-08-25 17:19:58 -0700

    Created appbar component and moved HTML for navigation into it.

---
+++ src/components/Appbar.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<nav>
+  <ol>
+    <li><a href="/">Home</a></li>
+    <li><a href="/about">About us</a></li>
+    <li><a href="plan">Create your plan</a></li>
+  </ol>
+</nav>
