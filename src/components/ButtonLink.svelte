8d2c40fed9c3d40465966ca066c01b44837e6166
astnio <github@mail.astn.io>
2025-08-28 13:16:31 -0700

    Realizing that I cannot pass functions into framework components, I decided to change the button to just be a link and renamed it to reflect that. Added more variables with more semantic meaning.

---
+++ src/components/ButtonLink.svelte
@@ -0,0 +1,49 @@
+<script>
+  let { children, link } = $props();
+</script>
+
+<a href={link} class="button btn-primary">
+  {@render children?.()}
+</a>
+
+<style lang="scss">
+  @use '/src/styles/variables' as *;
+
+  a.button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    border: none;
+    border-radius: 6pt;
+
+    padding-block: 1rem;
+    padding-inline: 0.5rem;
+
+    margin: 0;
+
+    width: 100%;
+    max-width: 12rem;
+
+    font-family: 'Fraunces', serif;
+    font-weight: 700;
+    font-size: 1rem;
+    text-decoration: none;
+
+    cursor: pointer;
+
+    transition-property: background-color;
+    transition-duration: 0.1s;
+    transition-timing-function: ease-out;
+  }
+
+  a.button.btn-primary {
+    background-color: $clr-primary;
+    color: $clr-light-cream;
+  }
+
+  a.button.btn-primary:active,
+  a.button.btn-primary:hover {
+    background-color: $clr-primary-active;
+  }
+</style>
