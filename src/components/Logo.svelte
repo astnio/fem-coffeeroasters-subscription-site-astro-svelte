755267741c37840e1fe0e9819844f7b897b92347
astnio <github@mail.astn.io>
2025-09-03 11:13:11 -0700

    Added dark and light logos. Refactored the logo component to handle dark/light mode. Styled footer

--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,7 +1,12 @@
 <script>
-  import logoImage from '../assets/shared/desktop/logo.svg';
+  import logoImageDark from '../assets/shared/desktop/logo.svg';
+  import logoImageLight from '../assets/shared/desktop/logo_light.svg';
+  let { darkMode } = $props();
 </script>
 
 <a href="/">
-  <img src={logoImage.src} alt="Coffeeroasters Logo" />
+  <img
+    src={darkMode ? logoImageLight.src : logoImageDark.src}
+    alt="Coffeeroasters Logo"
+  />
 </a>
